dd263351afed3e30929c6d6a0c6ef0bd2b826108
Cherie Williams <cherie@getpebble.com>
2013-11-21 16:31:32 -0800

    Releasing JS configuration demo for BETA2

---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,18 @@
+var initialized = false;
+
+Pebble.addEventListener("ready", function() {
+  console.log("ready called!");
+  initialized = true;
+});
+
+Pebble.addEventListener("showConfiguration", function() {
+  console.log("showing configuration");
+  Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  console.log("configuration closed");
+  // webview closed
+  var options = JSON.parse(decodeURIComponent(e.response));
+  console.log("Options = " + JSON.stringify(options));
+});
