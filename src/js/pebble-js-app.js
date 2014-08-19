33f901049a04f16855cb7092c85c3357c6d1b6cd
antgiant <antgiant@gmail.com>
2014-08-19 13:24:33 -0400

    Remember Options and Pass them to Config

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,4 +1,5 @@
 var initialized = false;
+var options = {};
 
 Pebble.addEventListener("ready", function() {
   console.log("ready called!");
@@ -7,12 +8,12 @@ Pebble.addEventListener("ready", function() {
 
 Pebble.addEventListener("showConfiguration", function() {
   console.log("showing configuration");
-  Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html');
+  Pebble.openURL('http://assets.getpebble.com.s3-website-us-east-1.amazonaws.com/pebble-js/configurable.html?'+encodeURIComponent(JSON.stringify(options)));
 });
 
 Pebble.addEventListener("webviewclosed", function(e) {
   console.log("configuration closed");
   // webview closed
-  var options = JSON.parse(decodeURIComponent(e.response));
+  options = JSON.parse(decodeURIComponent(e.response));
   console.log("Options = " + JSON.stringify(options));
 });
